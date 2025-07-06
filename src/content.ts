2689bbbfb2914385d56a30fd9f3a46acd8fa02a6
beygi <mahdyb@gmail.com>
2025-07-06 19:35:00 +0300

    display errors

--- src/content.ts
+++ src/content.ts
@@ -32,13 +32,7 @@ browser.runtime.onMessage.addListener((message) => {
         `);
   }
   if (message.command === 'show-error') {
-    setContainerHtml(`
-    <div class="flex items-center justify-center w-16 h-16 bg-accent rounded-full" >
-        <svg class="w-8 h-8 text-accent-content" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126z" />
-        </svg>
-    </div>
-        `);
+    showTranslationModal(message.text);
   }
   if (message.command === 'show-modal') {
     showTranslationModal(message.text);
